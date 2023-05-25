e045a6fb748003553d7e0bdba33b4aca928d0d65
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2023-05-25 18:11:24 +0200

    fix: enable_service_monitor variable propagation from flavors to base module

--- scaleway/main.tf
+++ scaleway/main.tf
@@ -5,9 +5,10 @@ module "traefik" {
   base_domain      = var.base_domain
   argocd_namespace = var.argocd_namespace
 
-  target_revision = var.target_revision
-  namespace       = var.namespace
-  app_autosync    = var.app_autosync
+  target_revision        = var.target_revision
+  namespace              = var.namespace
+  enable_service_monitor = var.enable_service_monitor
+  app_autosync           = var.app_autosync
 
   helm_values = concat(local.helm_values, var.helm_values)
 }
