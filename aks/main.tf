c537d85fd0d7a68faebf495793ed0561048d6448
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-09-15 14:16:13 +0200

    feat: add standard variables and variable to add labels to Argo CD app

--- aks/main.tf
+++ aks/main.tf
@@ -23,6 +23,7 @@ module "traefik" {
   base_domain            = var.base_domain
   argocd_namespace       = var.argocd_namespace
   argocd_project         = var.argocd_project
+  argocd_labels          = var.argocd_labels
   destination_cluster    = var.destination_cluster
   target_revision        = var.target_revision
   namespace              = var.namespace
