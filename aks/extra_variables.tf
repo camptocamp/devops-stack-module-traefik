19eb83ad3167409b789fe52ac4adb2c536d35e0a
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-12-21 19:41:54 +0100

    feat(aks)!: remove DNS resources from this module

--- aks/extra_variables.tf
+++ aks/extra_variables.tf
@@ -1,9 +1,1 @@
-variable "node_resource_group_name" {
-  description = "The aks managed cluster resource group name."
-  type        = string
-}
 
-variable "dns_zone_resource_group_name" {
-  description = "The Azure DNS zone's resource group name."
-  type        = string
-}
